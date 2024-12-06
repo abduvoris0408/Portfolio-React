99cef3785c4461fb582d9bb06c21e623335950f2
abduvoris0408 <abduvorismominov20408@gmail.com>
2024-12-06 22:57:31 +0500

    upatde nav linkd

--- src/components/NavbarItems.jsx
+++ src/components/NavbarItems.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavbarItems = ({ name, icon, link = "" }) => {
     return (
         <div>
             <li>
-                <a
-                    href={link}
+                <Link
+                    to={link}
                     className=" flex items-center gap-1 rounded-full border-[1px] border-slate-500 p-2 hover:border-green-500 cursor-pointer px-4 hover:text-green-500 duration-300 xs:text-[16px] xs:p-2 "
                 >
                     {" "}
                     {icon}
                     <p>{name}</p>
-                </a>
+                </Link>
             </li>
         </div>
     );
